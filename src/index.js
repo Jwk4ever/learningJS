794ab0584f649d3cb67051e0f35ffcd95d319d8d
Jwk4ever <734810239@qq.com>
2018-01-22 22:49:23 +0800

    first submit from school
    add a new method

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello MEITUAN');
+//console.log('hello MEITUAN');
+
+let Tool = (function(){
+	
+	let firstMethod = () => 'Hello !'
+
+	return {
+		firstMethod: firstMethod
+	}
+})()
+
+console.log(Tool.firstMethod());
