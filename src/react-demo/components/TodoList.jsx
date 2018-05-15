4dcd4a8e36da3143a12ba92ddca78a81a8dafcc0
jiaweikang <734810239@qq.com>
2018-05-15 19:19:18 +0800

    添加 throttle 函数

--- src/react-demo/components/TodoList.jsx
+++ src/react-demo/components/TodoList.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import TodoItem from './Todo.jsx'
 
 export default class TodoList extends Component {
+	// shouldComponentUpdate (){
+
+	// }
 	render (){
 		let { todos, toggleTodo} = this.props;
 		return (
--- src/test/index.jsx
+++ src/test/index.jsx
@@ -1,37 +1,119 @@
-import { wait } from '../utils';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-class A {
-	@loading
-	async get (){
-		let result = await wait();
-		console.log(result);
+class App extends Component {
+	constructor (props){
+		super(props);
+		this.onClick = this.onClick.bind(this);
+		this.state = {
+			counter: 0
+		}
 	}
-	// @loading
-	// sayName (){
-	// 	console.log('test');
-	// }
+	onClick (){
+		//e.preventDefault();
+		this.setState((prevState, nextProps) => {
+			console.trace('setState');
+			return {
+				counter: prevState.counter + 1
+			}
+		})
+	}
+	render() {
+		console.trace('render App')
+		return (<div>
+			<Counter counter = {this.state.counter}/>
+			<Button onClick = {this.onClick}/>
+		</div>)
+	}
 }
 
-// function loading(target, name, descriptor){
-// 	let _val = descriptor.value;
-// 	return descriptor;
-// }
-
-function loading(target, name, descriptor){
-	let _val = descriptor.value;
-	_val(); 
-	return descriptor;
+class Counter extends Component {
+	constructor (props){
+		super(props);
+		//console.log('Counter constructor');
+	}
+	static getDerivedStateFromProps() {
+		//console.log('Counter getDerivedStateFromProps');
+	}
+	componentWillMount (){
+		//console.log('Counter componentWillMount');
+	}
+	componentDidMount (){
+		//console.trace('Counter componentDidMount');
+	}
+	shouldComponentUpdate (){
+		//console.trace('Counter shouldComponentUpdate');
+		return true;
+	}
+	componentWillReceiveProps (){
+		//console.log('Counter componentWillRecceiveProps');
+	}
+	getSnapshotBeforeUpdate (){
+		//console.log('Counter getSnapshotBeforeUpdate');
+	}
+	componentDidUpdate (){
+		//console.trace('Counter ComponentDidUpdate');
+	}
+	render() {
+		console.trace('render Counter');
+		return (<div
+				style = {{ height:'30px', background: 'skyblue'}}
+			>
+				{this.props.counter}
+			</div>)
+	}
 }
 
-let a = new A();
+class Button extends Component {
+	constructor (props){
+		super(props);
+		this.clickhandler = this.clickhandler.bind(this);
+	}
+	clickhandler (e){
+		e.preventDefault();
+		this.props.onClick();
+	}
+	static getDerivedStateFromProps() {
+		// console.log('Button getDerivedStateFromProps');
+	}
+	componentWillMount (){
+		// console.log('Button componentWillMount');
+	}
+	componentDidMount (){
+		// console.trace('Button componentDidMount');
+	}
+	shouldComponentUpdate (){
+		// console.log('Button shouldComponentUpdate');
+		return true;
+	}
+	componentWillReceiveProps (){
+		// console.log('Button componentWillRecceiveProps');
+	}
+	getSnapshotBeforeUpdate (){
+		// console.log('Button getSnapshotBeforeUpdate');
+	}
+	componentDidUpdate (){
+		// console.log('Button ComponentDidUpdate');
+	}
+	render (){
+		console.log('render Button');
+		return (<button
+				onClick = {this.clickhandler}
+			>点击</button>)
+	}
+}
 
-a.sayName();
+class Test extends Component {
+	render (){
+		console.trace("test");
+		return <div>{'test'}</div>
+	}
+}
+
+render(
+	<App/>,
+	document.getElementById('app')
+	);
 
 
-var a = new Promise((resolve) => {
-	resolve({a: 1});
-})
 
-a.then((val) => {
-	console.log(val.a);
-})
